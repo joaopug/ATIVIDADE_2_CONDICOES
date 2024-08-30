5363e4553d2dbe37e7c6f86f9341e469f0443115
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2024-08-29 21:30:39 -0300

    Ajustes

--- src/Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impostos.java
+++ src/Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impostos.java
@@ -31,6 +31,10 @@ public class Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impost
         double valorImp = (imposto * salarioBruto);
         double salarioLiq = ((salarioBruto - (imposto * salarioBruto)) + (bonus * salarioBruto));
 
-        System.out.println("O funcionário recebe R$ " + salarioBruto + " e também trabalha na empresa há " + anosTrab + " ano(s).\n" + "Por conta disso, ele tem direito há um bônus de " + bonus * 100 + "% sobre o salário, resultando em R$ " + valorBns + " que ele deve receber.\n" + "Tudo seria maravilhoso se fosse só isso, mas infelizmente\n" + "precisa-se descontar " + imposto * 100 + "% do salário bruto devido a impostos, ou seja, R$ " + valorImp + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + "R$ " + salarioLiq);
+        if (bonus > 0.05) {
+            System.out.println("O funcionário recebe R$ " + salarioBruto + " e também trabalha na empresa há " + anosTrab + " ano(s).\n" + "Por conta disso, ele tem direito há um bônus de " + bonus * 100 + "% sobre o salário, resultando em R$ " + valorBns + " que ele deve receber.\n" + "Tudo seria maravilhoso se fosse só isso, mas infelizmente\n" + "precisa-se descontar " + imposto * 100 + "% do salário bruto devido a impostos, ou seja, R$ " + valorImp + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + "R$ " + salarioLiq);
+        } else {
+            System.out.println("O funcionário recebe R$ " + salarioBruto + " e também trabalha na empresa há " + anosTrab + " ano(s).\n" + "Por não trabalhar no mínimo 5 anos, ele não terá direito a bônus.\n" + "Infelizmente precisa-se descontar " + imposto * 100 + "% do salário bruto devido a impostos, ou seja, R$ " + valorImp + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + "R$ " + salarioLiq);
+        }
     }
 }
