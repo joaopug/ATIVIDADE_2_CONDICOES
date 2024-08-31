94e190cf966bd619f39d435e9c86255ed6ca2319
joaopug <charmandercharmandesco@gmail.com>
2024-08-31 18:56:19 -0300

    Melhoramento do código

--- src/Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impostos.java
+++ src/Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impostos.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impostos {
@@ -8,7 +9,7 @@ public class Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impost
 
         Scanner analise = new Scanner(System.in);
 
-        System.out.println("Digite o salário bruto do funcionário");
+        System.out.println("Digite o salário bruto do funcionário:");
         double salarioBruto = analise.nextDouble();
         System.out.println("Quanto anos o funcionário trabalha na empresa?");
         int anosTrab = analise.nextInt();
@@ -27,14 +28,20 @@ public class Atividade_11_Sistema_de_Calculo_de_Salario_com_Bonificacao_e_Impost
             imposto = 0.10;
         }
 
+        String padrao = "R$ ###,##0.00";
+        DecimalFormat df = new DecimalFormat(padrao);
+        String padrao2 = "0";
+        DecimalFormat df2 = new DecimalFormat(padrao2);
+
         double valorBns = (bonus * salarioBruto);
         double valorImp = (imposto * salarioBruto);
         double salarioLiq = ((salarioBruto - (imposto * salarioBruto)) + (bonus * salarioBruto));
 
-        if (bonus > 0.05) {
-            System.out.println("O funcionário recebe R$ " + salarioBruto + " e também trabalha na empresa há " + anosTrab + " ano(s).\n" + "Por conta disso, ele tem direito há um bônus de " + bonus * 100 + "% sobre o salário, resultando em R$ " + valorBns + " que ele deve receber.\n" + "Tudo seria maravilhoso se fosse só isso, mas infelizmente\n" + "precisa-se descontar " + imposto * 100 + "% do salário bruto devido a impostos, ou seja, R$ " + valorImp + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + "R$ " + salarioLiq);
+        System.out.println("O funcionário recebe " + df.format(salarioBruto) + " e também trabalha na empresa há " + anosTrab + " ano(s).");
+        if (bonus >= 0.05) {
+            System.out.println("Por conta disso, ele tem direito há um bônus de " + df2.format(bonus * 100) + "% sobre o salário, resultando em " + df.format(valorBns) + " que ele deve receber.\n" + "Tudo seria maravilhoso se fosse só isso, mas infelizmente\n" + "precisa-se descontar " + df2.format(imposto * 100) + "% do salário bruto devido a impostos, ou seja, " + df.format(valorImp) + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + df.format(salarioLiq));
         } else {
-            System.out.println("O funcionário recebe R$ " + salarioBruto + " e também trabalha na empresa há " + anosTrab + " ano(s).\n" + "Por não trabalhar no mínimo 5 anos, ele não terá direito a bônus.\n" + "Infelizmente precisa-se descontar " + imposto * 100 + "% do salário bruto devido a impostos, ou seja, R$ " + valorImp + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + "R$ " + salarioLiq);
+            System.out.println("Por não trabalhar no mínimo 5 anos, ele não terá direito a bônus.\n" + "Infelizmente precisa-se descontar " + df2.format(imposto * 100) + "% do salário bruto devido a impostos, ou seja, " + df.format(valorImp) + ".\n" + "Feito os cálculos, o salário líquido ficará:\n" + df.format(salarioLiq));
         }
     }
 }
